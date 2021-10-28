89a1b88c9cd64aa3afe73d0f45a736ce15367c22
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2021-10-28 12:56:52 +0400

    Fix space"

--- RefOut/Program.cs
+++ RefOut/Program.cs
@@ -10,32 +10,27 @@ namespace RefOut
             // string arr = "Hello P202 Group" ;
             // Console.WriteLine(Text(reverse.Length));
             //Console.WriteLine(Text(arr));
-            Console.WriteLine(Text("Hello P202 Group."));
+            Console.WriteLine(Text("Hello P202 Group"));
         }
         static string Text(string reverse)
         {
+            string result = string.Empty;
             int k = 0;
             Console.WriteLine("Length=" + reverse.Length);
-            string result = string.Empty;
+            
             for (int j = 0; j <reverse.Length;j++)
             {
 
-                if (reverse[j] == (' ') || (j == reverse.Length-1) || (reverse[j] == ('.'))||(reverse[j] == ('?')))
+                if (reverse[j] == (' ') || (j == reverse.Length-1) || (reverse[j] == ('.'))|| (reverse[j] == (','))||(reverse[j] == ('?'))|| (reverse[j] == ('!')))
                 {
-                    for (int i = j-1; i >= k; i--)
+                    for (int i = j - 1; i >= k; i--)
                     {
                         result += reverse[i];
-                        if (i == 0)
-                        {
-                            result += " ";
-                        }
-                        if (j == reverse.Length + 1)
-                        {
-                            result += ".";
-                        }
+                       
                     }
-                    k = j;
-                }
+                  //  result + = reverse[j];
+                    k = j+1;                  
+                }     
             }
                 return result;
         }
